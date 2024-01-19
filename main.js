8466c26497ebe1e03dc355e2068fe195866b7ea6
Jakob Mehamed Burhan <jakob.mehamed@chasacademy.se>
2024-01-19 11:47:00 +0100

    feat savePageTitle

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 import {updateTime} from "/timeAndDate.js";
+import {title} from "/savePageTitle.js";
 
+const editables = document.querySelectorAll("[contenteditable]");
 
 updateTime();
 setInterval(updateTime, 1000);
+
+title(editables);
---
+++ savePageTitle.js
@@ -0,0 +1,20 @@
+
+
+
+export const title =(editables)=>{
+
+    // save edits
+    editables.forEach(el => {
+    el.addEventListener("blur", () => {
+        localStorage.setItem("dataStorage-" + el.id, el.innerText);
+    })
+    });
+
+    // once on load
+    for (let key in localStorage) {
+        if (key.includes("dataStorage-")) {
+            const id = key.replace("dataStorage-","");
+            document.querySelector("#" + id).innerText = localStorage.getItem(key);
+        }
+    }
+}
