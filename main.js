f8578fa30dda1ee0136014d39a0c3439fdd2097f
Jakob Mehamed Burhan <jakob.mehamed@chasacademy.se>
2024-01-23 05:14:19 +0100

    feat publicHolidays

--- main.js
+++ main.js
@@ -1,11 +1,32 @@
 import {updateTime} from "/timeAndDate.js";
-import {title} from "/saveEdit.js";
+import {edit} from "/saveEdit.js";
+import { publicHolidays } from "./publicHolidays";  
+
 
 const clock = document.querySelector('#time');
 const dates = document.querySelector('#date');
 const editables = document.querySelectorAll("[contenteditable]");
+const publicHolidaysId = document.querySelector("#PublicHolidays");
 
 updateTime(clock,dates);
 setInterval(()=>{updateTime(clock,dates)}, 1000);
+edit(editables);
+publicHolidays(publicHolidaysId);
 
-title(editables);
+// const submitForm = document.querySelector('#saveUrl');
+
+// export const addlink =(button)=>{
+
+// }
+
+
+// const addToStorage = (event)=>{
+//     event.preventDefault();
+//     const myFormData = new FormData(event.target);
+//     const formDataObj = {};
+//     myFormData.forEach((value, key) => (formDataObj[key] = value));
+//     console.log(formDataObj);
+// }
+// submitForm.addEventListener("submit", addToStorage)
+
+
---
+++ publicHolidays.js
@@ -0,0 +1,22 @@
+
+
+export const publicHolidays =(publicHoliday)=>{
+
+    let year = new Date();
+    axios.get(`https://date.nager.at/api/v3/PublicHolidays/${year.getFullYear()}/SE`)
+    .then((res)=> holidays(res))
+    .catch((err)=> console.log(err))
+
+    const holidays = (data)=>{
+        data.data.forEach((id)=>{
+            publicHoliday.innerHTML += `
+            <div class="rounded bg-light opacity-50 d-flex justify-content-center flex-wrap m-1 rounded p-2">
+                <p class="text-nowrap block mx-1">${id.date}</p>
+                <p class="text-capitalize block">${id.localName}</p>
+            </div>
+            `
+        })
+        
+    }
+
+}
